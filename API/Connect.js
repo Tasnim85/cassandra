fa83431927cb78f1e75aae893a05e5696483a942
AchRef <rahaliashraf732@gmail.com>
2023-12-12 12:44:00 +0100

    msg

--- API/Connect.js
+++
@@ -1,9 +0,0 @@
-function connectToCassandra(cl) {
-  return cl.connect().then(() => {
-    console.log('Connected to Cassandra database!');
-  }).catch((error) => {
-    console.error('Error connecting to Cassandra database:', error);
-  });
-}
-
-module.exports = connectToCassandra;
